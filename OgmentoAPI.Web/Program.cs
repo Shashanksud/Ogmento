2f2efbbfa6d8216bc371117efd76506a11f422c8
minal kose <minalkose000@gmail.com>
2024-09-23 14:48:27 +0530

    feat: Implement logging using serilog package
    - Add serilog logging info in appsetting.json
    - Add Logs folder which contains Logs.txt and Logs.json
    - Update code in starrtup.cs and program.cs

--- OgmentoAPI.Web/Program.cs
+++ OgmentoAPI.Web/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace OgmentoAPI.Web
 {
@@ -7,11 +10,31 @@ namespace OgmentoAPI.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Application starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "The application Failed to start correctly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
