7709161e2a60c68b1617e99df03a66f08aef515c
Shaquibkhan <shaquibkhan2003@gmail.com>
2024-09-30 11:26:29 +0530

    -temp changes

---
+++ OgmentoAPI.Domain.Client.Infrastructure/Repository/KioskRepository.cs
@@ -0,0 +1,28 @@
+﻿using OgmentoAPI.Domain.Client.Abstractions.Dto;
+using OgmentoAPI.Domain.Client.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgmentoAPI.Domain.Client.Infrastructure.Repository
+{
+    public class KioskRepository
+    {
+        private readonly ClientDBContext _context;
+
+        public KioskRepository(ClientDBContext context)
+        {
+            _context = context;
+        }
+
+        public int? GetKioskDetails(KioskDto kioskDto)
+        {
+          
+
+        }
+
+
+    }
+}
