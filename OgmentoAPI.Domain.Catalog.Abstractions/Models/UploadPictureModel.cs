b2ca97513c2faba2de41ddfcef93a43325c584a3
minal kose <minalkose000@gmail.com>
2024-10-22 16:30:00 +0530

    refactor: Update AddPicture() in PictureRepository.cs to compute MD5 hash
    - Rename PictureName to FileName in UploadPictureModel.cs
    - Update DeleteProduct() in ProductRepository.cs by adding a condition to check if rows are deleted

--- OgmentoAPI.Domain.Catalog.Abstractions/Models/UploadPictureModel.cs
+++ OgmentoAPI.Domain.Catalog.Abstractions/Models/UploadPictureModel.cs
@@ -3,7 +3,7 @@
 	public class UploadPictureModel
 	{
 		public string SkuCode {  get; set; }
-		public string PictureName {  get; set; }
+		public string FileName {  get; set; }
 		public string Base64EncodedData { get; set; }
 		public string MimeType { get; set; }
 	}
